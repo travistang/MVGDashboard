1d1a4a573f289baf908348bff5ac47b6fb2888d6
Travis Tang <ga86yam@mytum.de>
2018-08-10 13:57:04 +0200

    Configure store for production

--- app/store/configureStore.prod.js
+++ app/store/configureStore.prod.js
@@ -6,11 +6,15 @@ import { routerMiddleware } from 'react-router-redux';
 import rootReducer from '../reducers';
 import type { counterStateType } from '../reducers/counter';
 import createSagaMiddleware from 'redux-saga'
+import rootSaga from '../sagas'
+const sagaMiddleware = createSagaMiddleware()
+
 const history = createHashHistory();
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router,createSagaMiddleware());
+const enhancer = applyMiddleware(thunk, router,sagaMiddleware);
 
 function configureStore(initialState?: counterStateType) {
+  sagaMiddleware.run(rootSaga)
   return createStore(rootReducer, initialState, enhancer);
 }
 
