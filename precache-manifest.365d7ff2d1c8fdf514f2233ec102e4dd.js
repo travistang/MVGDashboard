dbebb7817c9fcf755324589a64ced4f59896fb8f
Travis <ga86yam@mytum.de>
2019-02-10 11:22:23 +0100

    Updates

---
+++ precache-manifest.365d7ff2d1c8fdf514f2233ec102e4dd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/MVGDashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "e272cce31e63ad386ffa",
+    "url": "/MVGDashboard/static/css/main.209ba421.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/MVGDashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4c3e3a1652f35ff860b8",
+    "url": "/MVGDashboard/static/js/1.4c3e3a16.chunk.js"
+  },
+  {
+    "revision": "4d98cafd8d3227e41e8e",
+    "url": "/MVGDashboard/static/js/runtime~main.4d98cafd.js"
+  },
+  {
+    "revision": "e272cce31e63ad386ffa",
+    "url": "/MVGDashboard/static/js/main.e272cce3.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/MVGDashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/MVGDashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/MVGDashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "4c3e3a1652f35ff860b8",
+    "url": "/MVGDashboard/static/css/1.dab816cb.chunk.css"
+  },
+  {
+    "revision": "374695d3b5a6186af2e1564b2fd2c187",
+    "url": "/MVGDashboard/index.html"
+  }
+];
